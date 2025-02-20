ce2dc02557c48f06a964ec66bd511291ca3da416
Julian Gomez <julian.gomeza@pragma.com.co>
2025-02-20 08:11:50 -0500

    Primer commit

---
+++ src/main/java/small_square_microservice/small_square/SmallSquareApplication.java
@@ -0,0 +1,13 @@
+package small_square_microservice.small_square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallSquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/small_square_microservice/small_square/SmallSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package small_square_microservice.small_square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
